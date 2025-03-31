8460236717c7929cf11461acba5fa281853a722f
胡予娟 <superstickman19.mc@gmail.com>
2025-04-01 01:36:02 +0800

    Update counter1.js

--- counter1.js
+++ counter1.js
@@ -1,4 +1,6 @@
-/*https://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascript
+/*
+111401546 胡予娟
+https://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascript
 Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 */
 var createCounter = function(n) {   //外部函式 createCounter(n) 
